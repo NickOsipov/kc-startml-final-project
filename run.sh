28cc6878427de0885b7a6a7a5f0c3699f5a05495
NickOsipov <nick.osipov.91@gmail.com>
2024-11-15 21:26:48 +0000

    fix: run scripts

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-docker build -t kc:final-project .
+docker build -t kc-startml-final-project:latest .
 
-docker stop kc-final-project
+docker stop kc-startml-final-project
 
-docker run --rm -v .:/app --name kc-final-project -p 8000:8000 kc:final-project
+docker run --rm -v .:/app --name kc-startml-final-project -p 8000:8000 kc-startml-final-project:latest
